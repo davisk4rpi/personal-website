39150e3ce75b8fcea813e8713821d81424ec6c69
davisk4rpi@gmail.com <davisk4rpi@gmail.com>
2017-02-21 19:34:42 -0800

    First refactor to application.html.erb

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Kieran P. Davis"
+  end
+
   test "should get home" do
     get root_url
     assert_response :success
-    assert_select "title", "Home | Kieran P. Davis"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get resume" do
     get resume_url
     assert_response :success
-    assert_select "title", "Resume | Kieran P. Davis"
+    assert_select "title", "Resume | #{@base_title}"
   end
 
   test "should get about" do
     get about_url
     assert_response :success
-    assert_select "title", "About | Kieran P. Davis"
+    assert_select "title", "About | #{@base_title}"
   end
 
 end
